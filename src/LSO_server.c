056966cff91ba570fbc874f318b8d07ede3c2911
Francesco Junior Iaccarino <59925828+frankkk1013@users.noreply.github.com>
2022-01-19 18:19:08 +0100

    fix socket

--- src/LSO_server.c
+++ src/LSO_server.c
@@ -114,7 +114,7 @@ int createSocket()
 	bool error;
 	struct sockaddr_in serverAddress;
 	memset(&serverAddress, '0', sizeof(serverAddress));
-	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_family = PF_INET;
 	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	//inserire porta
